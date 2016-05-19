cfa53dd12471aeb9cafc81f153a5564a5d1e8189
mendoz <purplex@protonmail.ch>
2016-05-19 16:36:17 -0700

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,6 @@
+var myFavNum = 7; // Integer
+var notMyFavNum = '2'; // String
+var bonus =  ((parseInt(notMyFavNum) + myFavNum) / 2);
+
+console.log('My favorite number is: ' + myFavNum + ', it\'s not: ' + notMyFavNum + '\n');
+console.log('The average of ' + myFavNum + ' & ' + notMyFavNum + ' is = ' + bonus);
