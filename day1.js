67b0b534a330c43afca8b4d79fa2a613cc626ebd
mendoz <purplex@protonmail.ch>
2016-05-19 16:56:05 -0700

    Add files via upload

--- day1.js
+++ day1.js
@@ -1,6 +1,8 @@
 var myFavNum = 7; // Integer
 var notMyFavNum = '2'; // String
-var bonus =  ((parseInt(notMyFavNum) + myFavNum) / 2); // Average of myFavNum & notMyFavNum
+var bonus =  ((parseInt(notMyFavNum) + myFavNum) / 2);
 
 console.log('My favorite number is: ' + myFavNum + ', it\'s not: ' + notMyFavNum + '\n');
 console.log('The average of ' + myFavNum + ' & ' + notMyFavNum + ' is = ' + bonus);
+console.log(typeof myFavNum);
+console.log(typeof notMyFavNum);
