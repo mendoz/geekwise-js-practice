6cca190015c82e67c30a888ccb57c14a5b0c6bbe
mendoz <purplex@protonmail.ch>
2016-05-19 17:57:50 -0700

    Add files via upload

---
+++ day2 warmup.js
@@ -0,0 +1,20 @@
+var A = 7;
+var B = "String";
+var C;
+var isOn = false;
+
+console.log(A + '\n' + B + '\n' + C + '\n' + isOn);
+
+// d = "Test"; Error: undefined variable
+
+var favMovies = ['Chicken Little', 'Wall-E', 'Lion King', 'Monsters Inc.'];
+
+var myFavStuff = {
+	Foods: ['Pizza', 'Tacos', 'Pasta'],
+	Movies: ['Chicken Little', 'Wall-E', 'Lion King'],
+	Books: ['Hatchet', 'My Side of the Mountain'],
+	Games: ['Call of Duty 4: Modern Warfare', 'Metal Gear Solid 3: Snake Eater', 'Minecraft'],
+}
+
+console.log(myFavStuff.Games[1]);
+   
