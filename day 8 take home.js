3be49165f397f637bdf66b01154dc49c88a6694a
mendoz <purplex@protonmail.ch>
2016-06-11 15:26:13 -0700

    Add files via upload

--- day 8 take home.js
+++ day 8 take home.js
@@ -47,14 +47,26 @@ var movieConstructor = function (Title, runTimeInMin, Year, Genre, Desc) {
 	this.Time = parseInt(runTimeInMin); 
 	this.Year = parseInt(Year);
 	this.Genre = Genre;
-	this.Desc = (Desc) ? Desc : 'N/A';	// Sets description value to N/A if empty 
+	this.Desc = Desc || 'N/A';	// Sets description value to N/A if empty 
+    this.isAvailable = true;
 	this.runTimeInHours = function () { 
 		return parseInt(this.Time / 60) + 'h ' + this.Time % 60 + 'm';	// Divide by 60 to get hours modulus 60 to get minutes
 	}
 	this.Preview = function () {		// Slices the first 50 characters of description then adds ... 
 		return (this.Desc.length >= 50) ? this.Desc.slice(0, 50) + '...' : this.Desc;
 	}
-	
+    this.Info = function () {
+        return this.Title + ' plot description: \n' + this.Preview() + '\nLength: ' + this.runTimeInHours() + '\nReleased: ' + this.Year + '\nGenre: ' + this.Genre;
+    }
+    this.Update = function (Time, Year, Genre, Desc) {
+        this.Time = parseInt(Time) || this.Time;
+        this.Year = parseInt(Year) || this.Year; // Keeps value the same if new value is empty 
+        this.Genre = Genre || this.Genre;
+        this.Desc = Desc || this.Desc;      
+    }
+    this.checkInOrOut = function () {
+        this.isAvailable = !this.isAvailable;
+    }
 };
 
 var submit = document.getElementById('newMovieForm'),
@@ -62,40 +74,47 @@ var submit = document.getElementById('newMovieForm'),
 	newMovies = []; 					// Creates an empty movie array for to store movie OBJs
 
 submit.addEventListener('submit', function (evt) { 
-	evt.preventDefault();					// Prevents page reload after submit event 
-	if (!submit.movieTitle.value.trim()) { 	// Checks if user is submiting an empty title
-		return alert('Please enter a valid movie title!'); // Prevents movie OBJ from being constructed if title is empty 
-	}
-	for (var i in newMovies) {
-		if (submit.movieTitle.value.toLowerCase().trim() == newMovies[i].Title.toLowerCase()) { // Checks if input title already exists 
-			return alert(newMovies[i].Title + ' is already in our database!');
-		}
-	}
-	var currentTitle = capitalize(submit.movieTitle.value), 
+	evt.preventDefault();	// Prevents page reload after submit event 
+    var currentTitle = capitalize(submit.movieTitle.value), 
 		currentTime = submit.runTime.value,
 		currentYear = submit.yearReleased.value, // Stores input values into local variables
 		currentGenre = submit.genre,
-		currentDesc = submit.desc.value;
-	for (var i in currentGenre) {				 // Loops over every element in the currentGenre array 
+		currentDesc = submit.desc.value.trim();
+    for (var i in currentGenre) {				 // Loops over every element in the currentGenre array 
 		if (currentGenre[i].checked) {			
 			currentGenre = currentGenre[i].value; // Overwrites the currentGenre array with the checked value
 			break;
 		}
 	}
-	
+    if (!currentTitle) { 	// Checks if user is submiting an empty title
+		return alert('Please enter a movie title!'); // Prevents movie OBJ from being constructed if title is empty 
+	}
+	for (var i in newMovies) {
+		if (currentTitle.toLowerCase() == newMovies[i].Title.toLowerCase()) { // Checks if input title already exists 
+            if (confirm(newMovies[i].Title + ' is already in our database!\nDid you want to update it\'s information?')) {
+               return newMovies[i].Update(currentTime, currentYear, currentGenre, currentDesc); // Prompts user if they want to update the movie info  
+            } else {
+                return; // Prevents duplicate movie from being constructed
+            }
+		}
+	}
 	// Creates a movie OBJ then adds it to the newMovies array
 	newMovies.push(new movieConstructor(currentTitle, currentTime, currentYear, currentGenre, currentDesc)); 
-	var li = e('li', currentTitle, {}, {color: 'green'}); // Creates a new list item with a value of the user entered movie title
+	var li = e('li', currentTitle, {id: currentTitle}, {color: 'green'}); // Creates a new list item with a value of the entered movie title
 	ul.appendChild(li);
 	document.body.appendChild(ul);
 	console.log(newMovies);
 } );
 
-
 ul.addEventListener('click', function (evt) {
 	for (var i in newMovies) {
-		if (newMovies[i].Title.toLowerCase() == evt.target.textContent.toLowerCase()) {	 // Checks if li user clicked on matches any movie
-			alert(newMovies[i].Title + ' plot summary: \n' + newMovies[i].Preview() + '\n\nLength: ' + newMovies[i].runTimeInHours() + '\nReleased: ' + newMovies[i].Year + '\nGenre: ' + newMovies[i].Genre);
+		if (newMovies[i].Title.toLowerCase() == evt.target.textContent.toLowerCase()) {	 // Checks if li user clicked on matches a movie in our Array
+            if (confirm(newMovies[i].Info() + '\n\nDid you want to check this movie ' + ((newMovies[i].isAvailable) ? 'out?' : 'in?'))) { 
+                newMovies[i].checkInOrOut(); // Flips the isAvailable variable 
+                var li = document.getElementById(newMovies[i].Title), // Gets li the user clicked on
+                    color = (newMovies[i].isAvailable) ? 'color: green;' : 'color: red;'; // Checks if movie is available to set text color
+                li.setAttribute('style', color); // Sets text color 
+            }
 		}
 	}
-});
+} );
